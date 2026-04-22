4910b67bc56533ee776a85913994e860ef5029c0
mahasiswa229 <mafaqih1945@gmail.com>
2026-04-22 10:36:56 +0700

    user service

---
+++ src/main/java/com/praktikumDB/deploy/service/UserService.java
@@ -0,0 +1,40 @@
+package com.praktikumDB.deploy.service;
+
+import com.praktikumDB.deploy.model.User;
+import com.praktikumDB.deploy.repository.UserRepository;
+
+import java.util.List;
+
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addUser(User request) {
+        return userRepository.save(request);
+    }
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+
+    public User updateUser(String id, User request) {
+        User existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            existingUser.setName(request.getName());
+            existingUser.setNim(request.getNim());
+            return userRepository.save(existingUser);
+        }
+        return null;
+    }
+}
